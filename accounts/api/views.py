0e78694a9eb9e0edf846200606e13e37ab1ac10d
Penny <cloverz1128@gmail.com>
2025-04-10 00:00:09 +1000

    login status

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
-
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -11,3 +12,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+	
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {'has_logged_in': request.user.is_authenticated}  
+        if request.user.is_authenticated:  
+            data['username'] = UserSerializer(request.user).data
+        return Response(data)
