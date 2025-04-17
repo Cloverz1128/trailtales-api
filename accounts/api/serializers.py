2c7ee661bb5e09af1cdd1b67be9c02116f304311
Penny <cloverz1128@gmail.com>
2025-04-17 14:41:34 +1000

    login

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,8 +3,12 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from accounts.api.serializers import UserSerializer
-from django.contrib.auth import logout as django_logout
+from accounts.api.serializers import UserSerializer, LoginSerializer
+from django.contrib.auth import (
+    logout as django_logout,
+    login as django_login,
+    authenticate as django_auth
+)
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -15,16 +19,52 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class AccountViewSet(viewsets.ViewSet):
-    serializer_class = UserSerializer
+    serializer_class = LoginSerializer
 	
     @action(methods=['GET'], detail=False)
     def login_status(self, request):
         data = {'has_logged_in': request.user.is_authenticated}  
         if request.user.is_authenticated:  
-            data['username'] = UserSerializer(request.user).data
+            data['username'] = LoginSerializer(request.user).data
         return Response(data)
     
     @action(methods=['POST'], detail = False)
     def logout(self, request):
         django_logout(request)
         return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        # get username and password
+        serializer = LoginSerializer(data=request.data)
+
+        if not serializer.is_valid():  
+            return Response({  
+                "success": False,  
+                "message": "Please check input",  
+                "errors": serializer.errors,  
+            }, status=400)  
+
+        username = serializer.validated_data['username']  
+        password = serializer.validated_data['password']
+
+        # if user is not exist
+        if not User.objects.filter(username=username).exists():
+            return Response({  
+                "success": False,  
+                "message": "User does not exist",  
+            }, status=400)
+
+        # validation ok, login
+        user = django_auth(username=username, password=password)  
+        if not user or user.is_anonymous:  
+            return Response({  
+                "success": False,  
+                "message": "username and password does not match",  
+            }, status=400)  
+        
+        django_login(request, user) #generate and set login cookie and session
+        return Response({  
+            "success": True,  
+            "user": UserSerializer(instance=user).data
+        })
