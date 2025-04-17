ecf5d2859c3f5d5f5a650aeb7f24fdb9be3772a4
Penny <cloverz1128@gmail.com>
2025-04-17 13:32:38 +1000

    logout

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
+from django.contrib.auth import logout as django_logout
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -14,6 +15,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class AccountViewSet(viewsets.ViewSet):
+    serializer_class = UserSerializer
 	
     @action(methods=['GET'], detail=False)
     def login_status(self, request):
@@ -21,3 +23,8 @@ class AccountViewSet(viewsets.ViewSet):
         if request.user.is_authenticated:  
             data['username'] = UserSerializer(request.user).data
         return Response(data)
+    
+    @action(methods=['POST'], detail = False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
