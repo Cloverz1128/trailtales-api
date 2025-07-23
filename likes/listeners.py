5f33761d8272915bf5905de9cdeb67574f6c44a0
Penny <cloverz1128@gmail.com>
2025-07-23 21:50:30 +1000

    denormalize likes-count and comments-count, and redis counts seperately

---
+++ likes/listeners.py
@@ -0,0 +1,29 @@
+from utils.redis_helper import RedisHelper
+
+
+def incr_likes_count(sender, instance, created, **kwargs):
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    if not created: # not create, only update, return
+        return 
+    
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # todo: comment's like
+        return
+    
+    Tweet.objects.filter(id=instance.object_id).update(likes_count=F('likes_count') + 1)
+    RedisHelper.incr_count(instance.content_object, 'likes_count')
+
+def decr_likes_count(sender, instance, **kwargs):
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # todo: comment's like
+        return
+    
+    Tweet.objects.filter(id=instance.object_id).update(likes_count=F('likes_count') - 1)
+    RedisHelper.decr_count(instance.content_object, 'likes_count')
--- tweets/models.py
+++ tweets/models.py
@@ -20,6 +20,10 @@ class Tweet(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # add two new denormalization field to avoid n plus one query
+    likes_count = models.IntegerField(default=0, null=True)
+    comments_count = models.IntegerField(default=0, null=True)
+
     class Meta:
         index_together = (('user', 'created_at'),) 
         ordering = ('user', '-created_at')
