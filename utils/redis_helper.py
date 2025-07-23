ec8836c5a83294577b47ecbc181ad36ad873bcc8
Penny <cloverz1128@gmail.com>
2025-07-23 21:57:58 +1000

    improvement of count in redis helper

--- utils/redis_helper.py
+++ utils/redis_helper.py
@@ -45,31 +45,42 @@ class RedisHelper:
         serialized_data = DjangoModelSerializer.serialize(obj)
         conn.lpush(key, serialized_data)
         conn.ltrim(key, 0, settings.REDIS_LIST_LENGTH_LIMIT - 1)
-    
+
     @classmethod
     def get_count_key(cls, obj, attr):
         return '{}.{}:{}'.format(obj.__class__.__name__, attr, obj.id)
-
+        
     @classmethod
     def incr_count(cls, obj, attr):
         conn = RedisClient.get_connection()
         key = cls.get_count_key(obj, attr)
-        if not conn.exists(key):
-            conn.set(key, getattr(obj, attr))
-            conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
-            return getattr(obj, attr)
-        return conn.incr(key)
+        
+        if conn.exists(key):
+            return conn.incr(key)
+
+        # back fill cache from db
+        # 不执行+1 操作keying必须保证调用incr_count 之前 obj.attr已经+1过了
+        obj.refresh_from_db()
+        conn.set(key, getattr(obj, attr))
+        conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
+        return getattr(obj, attr)
+        
+        
 
     @classmethod
     def decr_count(cls, obj, attr):
         conn = RedisClient.get_connection()
         key = cls.get_count_key(obj, attr)
-        if not conn.exists(key):
-            conn.set(key, getattr(obj, attr))
-            conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
-            return getattr(obj, attr)
-        return conn.decr(key)
-
+        if conn.exists(key):
+            return conn.decr(key)
+        # back fill cache from db
+        # 不执行-1 操作keying必须保证调用incr_count 之前 obj.attr已经-1过了
+        obj.refresh_from_db()
+        conn.set(key, getattr(obj, attr))
+        conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
+        return getattr(obj, attr)
+        
+        
     @classmethod
     def get_count(cls, obj, attr):
         conn = RedisClient.get_connection()
@@ -78,7 +89,7 @@ class RedisHelper:
         if count is not None:
             return int(count)
 
-        obj.refresh_from_db()
+        obj.refresh_from_db() 	# 确保数据是最新的
         count = getattr(obj, attr)
         conn.set(key, count)
         return count
